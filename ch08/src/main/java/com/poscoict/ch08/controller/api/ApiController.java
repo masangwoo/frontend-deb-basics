75b3087e370a980095075d937342b50a6f12581f
masangwoo <masw23@naver.com>
2022-02-28 12:54:45 +0900

    ajax #CRUD

--- ch08/src/main/java/com/poscoict/ch08/controller/api/ApiController.java
+++ ch08/src/main/java/com/poscoict/ch08/controller/api/ApiController.java
@@ -1,6 +1,7 @@
 package com.poscoict.ch08.controller.api;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -26,7 +27,7 @@ public class ApiController {
 	
 	@ResponseBody
 	@RequestMapping(value = "/json", method=RequestMethod.GET)
-	public Object json() {
+	public JsonResult json() {
 		GuestbookVo vo = new GuestbookVo();
 		vo.setNo(1L);
 		vo.setName("둘리");
@@ -38,4 +39,25 @@ public class ApiController {
 		return JsonResult.success(vo);
 		//return JsonResult.fail("Exception...");
 	}
+	
+	@ResponseBody
+	@RequestMapping(value = "/post01", method=RequestMethod.POST)
+	public JsonResult post01(GuestbookVo vo) {
+		System.out.println(vo);
+		//sersvice -> repository : DB insert 성공한 후 
+		vo.setNo(1L);
+		vo.setPassword("");
+		return JsonResult.success(vo);
+	}
+	
+	@ResponseBody
+	@RequestMapping(value = "/post02", method=RequestMethod.POST)
+	public JsonResult post02(@RequestBody GuestbookVo vo) {
+		System.out.println(vo);
+		//sersvice -> repository : DB insert 성공한 후 
+		vo.setNo(1L);
+		vo.setPassword("");
+		return JsonResult.success(vo);
+	}
+	
 }
--- ch08/src/main/java/com/poscoict/ch08/controller/test/ApiTestController.java
+++ ch08/src/main/java/com/poscoict/ch08/controller/test/ApiTestController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller("apiTestController")
 @RequestMapping("/test")
-public class ApiController {
+public class ApiTestController {
 	
 	@RequestMapping("/text")
 	public String text() {
@@ -20,5 +20,25 @@ public class ApiController {
 	@RequestMapping("/json01")
 	public String json01() {
 		return "json01";
+	}
+	
+	@RequestMapping("/json02")
+	public String json02() {
+		return "json02";
+	}
+	
+	@RequestMapping("/json03")
+	public String json03() {
+		return "json03";
+	}
+	
+	@RequestMapping("/json04")
+	public String json04() {
+		return "json04";
+	}
+	
+	@RequestMapping("/post01")
+	public String post01() {
+		return "post01";
 	}
 }
